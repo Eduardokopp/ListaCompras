78e1cc5986ca90c827bde381b42e4f0483fb6c52
Eduardo Kopp Santos <93801467+Eduardokopp@users.noreply.github.com>
2022-08-08 21:40:47 -0300

    Implementação de Localstorage
    Foi realizado a implementação do LocalStorage em Javascript assim mantendo a lista salva na maquina local sem uso de banco de dados

--- script.js
+++ script.js
@@ -5,62 +5,63 @@ var texto
 var saida
 var seExiste
 var produtos = Array()
-produtos["carnes"] = Array()
-produtos["frutas"] = Array()
-produtos["verduras"] = Array()
-produtos["graos"] = Array()
 
+
+//Função para ativar o botão de adicoinar ao clicar ENTER
+document.addEventListener("keypress", function (e) {
+    if (e.key === 'Enter') {
+        var btn = document.getElementById("botao");
+        btn.click();
+    }
+});
 function add() {
-
     nome = document.getElementById("nome").value;
     var form = document.getElementById("formulario");
-    // produtos["carnes"].push(nome);
-    if (document.getElementById("categorias").value == "selecione") {
-        alert("Selecione uma categoria para o cadastro")
+    let catSelecionada = document.getElementById("categorias").value
+
+    if (catSelecionada == "selecione" || nome == "") {
+        alert("Informe todos os dados")
         return
     }
-    if (nome == "") {
-        alert("digite o nome do produto")
-        return
+    if (existe(catSelecionada, nome)) {
+        alert("O item " + nome + " já existe na lista de " + catSelecionada)
     }
-    if (document.getElementById("categorias").value == "carnes") {
-        produtos["carnes"].push(nome)
-
-    } else if (document.getElementById("categorias").value == "frutas") {
-        produtos["frutas"].push(nome)
-    } else if (document.getElementById("categorias").value == "verduras") {
-        produtos["verduras"].push(nome)
-    } else if (document.getElementById("categorias").value == "graos") {
-        produtos["graos"].push(nome)
+    else {
+        switch (catSelecionada) {
+            case "carnes":
+                produtos["carnes"].push(nome)
+                localStorage.setItem('carnes', JSON.stringify(produtos["carnes"]))
+                break;
+            case "frutas":
+                produtos["frutas"].push(nome)
+                localStorage.setItem('frutas', JSON.stringify(produtos["frutas"]))
+                break;
+            case "verduras":
+                produtos["verduras"].push(nome)
+                localStorage.setItem('verduras', JSON.stringify(produtos["verduras"]))
+                break;
+            case "graos":
+                produtos["graos"].push(nome)
+                localStorage.setItem('graos', JSON.stringify(produtos["graos"]))
+                break;
+                console.log(produtos)
+        } //Fim do switch 
+        form.reset()
+        reload()
     }
-
-    form.reset();
-
-    exibir_carnes()
-    exibir_frutas()
-    exibir_Verduras()
-    exibir_graos()
-
-    console.log(produtos);
 }
-
 function exibir_carnes() {
-
     lista = document.createElement("ol");
-
     produtos["carnes"].forEach(function (item) {
         listaItem = document.createElement("li")
         texto = document.createTextNode(item)
-        listaItem.append(texto);
-        lista.append(listaItem);
+        listaItem.append(texto)
+        lista.append(listaItem)
     });
-
     saida = document.getElementById("carne_list");
     saida.innerHTML = "";
     saida.append(lista);
 }
-
-
 function exibir_frutas() {
     lista = document.createElement("ol");
     produtos["frutas"].forEach(function (item) {
@@ -68,14 +69,11 @@ function exibir_frutas() {
         texto = document.createTextNode(item)
         listaItem.append(texto);
         lista.append(listaItem);
-
     });
     saida = document.getElementById("frutas_list");
     saida.innerHTML = "";
     saida.append(lista);
 }
-
-
 function exibir_Verduras() {
     lista = document.createElement("ol");
     produtos["verduras"].forEach(function (item) {
@@ -83,14 +81,11 @@ function exibir_Verduras() {
         texto = document.createTextNode(item)
         listaItem.append(texto);
         lista.append(listaItem);
-
     });
     saida = document.getElementById("verduras_list");
     saida.innerHTML = "";
     saida.append(lista);
 }
-
-
 function exibir_graos() {
     lista = document.createElement("ol");
     produtos["graos"].forEach(function (item) {
@@ -98,9 +93,63 @@ function exibir_graos() {
         texto = document.createTextNode(item)
         listaItem.append(texto);
         lista.append(listaItem);
-
     });
     saida = document.getElementById("graos_list");
     saida.innerHTML = "";
     saida.append(lista);
+}
+function Limpar(categoria) {
+    switch (categoria) {
+        case 1:
+            localStorage.removeItem('carnes');
+            break;
+        case 2:
+            localStorage.removeItem('frutas');
+            break;
+        case 3:
+            localStorage.removeItem('verduras');
+            break;
+        case 4:
+            localStorage.removeItem('graos');
+            break;
+    }
+    reload()
+
+}
+function reload() {
+    //Pegando os dados do LOCALSTORAGE
+    produtos["carnes"] = JSON.parse(localStorage.getItem('carnes')) || []
+    produtos["frutas"] = JSON.parse(localStorage.getItem('frutas')) || []
+    produtos["verduras"] = JSON.parse(localStorage.getItem('verduras')) || []
+    produtos["graos"] = JSON.parse(localStorage.getItem('graos')) || []
+
+    //Exibindo cada um dos itens depois de recarregar o array
+    exibir_carnes()
+    exibir_frutas()
+    exibir_Verduras()
+    exibir_graos()
+}
+function existe(tipo, nome) {
+
+    switch (tipo) {
+        case 'carnes':
+            // se já existir retorna verdadeiro
+            return produtos["carnes"].includes(nome)
+            break;
+        case 'frutas':
+            return produtos["frutas"].includes(nome)
+            break;
+        case 'verduras':
+            return produtos["verduras"].includes(nome)
+            break;
+        case 'graos':
+            return produtos["graos"].includes(nome)
+            break;
+
+    }
+
+    //.indexOf(1)
+    // includes('Matheus')
+
+
 }
